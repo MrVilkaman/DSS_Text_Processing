926cee673dc587516d31ce48516096fdad65534a
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-05-16 12:17:20 +0300

    some rafactoring

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,7 +1,6 @@
+import datalayer.providers.DictionariesDPImpl
 import domain.DIManager
-import domain.Porter
-import domain.entity.WordsFrequency
-import domain.entity.revSort
+import domain.TextUtils
 import java.util.*
 
 /**
@@ -18,115 +17,42 @@ fun main(args: Array<String>) {
 
 
 	var rawText = textDP.getRawText()
-	splitBySentensies(rawText, dictionariesDP.getDictionaryAbbreviations())
-//	words = split(dictionariesDP.getDictionaryStopWords(), words)
-//	rawText = buildText(words)
-//	println("$rawText")
-//
-//	println("****")
-//	words = stemming(words)
-//	groupe(words)
+	showSentensies(dictionariesDP, rawText)
+
+	showWordsHandle(dictionariesDP, rawText)
 
 	println("exit")
 }
 
-fun splitBySentensies(rawText: String, dictAbb: List<String>) {
-	val carretWord = "\n*"
-
-	val sb = StringBuilder(rawText)
-	var lastIndex = -1;
-	val listChars = Arrays.asList('.', '!', '?')
-	for (ch in listChars)
-		do {
-			lastIndex = sb.indexOf(ch, lastIndex + 1)
-			val flag = lastIndex != -1;
-			if (flag) {
-				sb.insert(lastIndex + 1, carretWord)
-			}
-		} while (flag)
-
-	lastIndex = -1
-	do {
-		lastIndex = sb.indexOf(carretWord, lastIndex + 1)
-		for (word in dictAbb) {
-
-			val flag = matchStr(sb, lastIndex, word);
-			if (flag) {
-				sb.replace(lastIndex, lastIndex + carretWord.length, "")
-				break
-			}
-		}
-	} while (lastIndex != -1)
-
-
-	println(sb.toString())
+private fun showWordsHandle(dictionariesDP: DictionariesDPImpl, rawText: String) {
+	var words = TextUtils.splitByWords(dictionariesDP.getDictionaryStopWords(), rawText)
+	println("Текст без \"мусора\":")
+	println()
+	println(buildText(words))
+	words = TextUtils.stemming(words)
+	useGroupe(words)
 }
 
-fun matchStr(sb: StringBuilder, lastIndex: Int, word: String): Boolean {
-	val length = word.length
-	var startIndex = lastIndex - length * 2
-	startIndex = if (startIndex < 0) 0 else startIndex
-	for (strIndex in startIndex..lastIndex) {
-		var flag = false
-		for (wordIndex in 0..length - 1) {
-			val c = sb[strIndex + wordIndex]
-			val c1 = word[wordIndex]
-			if (c == c1) {
-				flag = true
-			} else {
-				flag = false
-				break
-			}
-		}
-		if (flag) return true
-	}
-	return false
+private fun showSentensies(dictionariesDP: DictionariesDPImpl, rawText: String) {
+	val sentensies = TextUtils.splitBySentensies(rawText, dictionariesDP.getDictionaryAbbreviations())
+	println("Выделенеи предложений: ")
+	println(sentensies)
+	println("****")
+	println()
 }
 
-fun stemming(words: ArrayList<String>): ArrayList<String> {
-	val list = ArrayList<String>()
-	words.forEach { list.add(Porter.stem(it)) }
-	return list;
-}
 
-private fun groupe(words: ArrayList<String>) {
-	val list = ArrayList(words)
-
-	var groupBy = list.groupBy { it }
-	val mapp: ArrayList<WordsFrequency> = ArrayList<WordsFrequency>()
-	groupBy.forEach { mapp.add(WordsFrequency(it.key, it.value.size)) }
-	mapp.revSort()
-	mapp.removeIf { it.count == 1 }
-
+fun useGroupe(words: ArrayList<String>) {
+	val mapp = TextUtils.groupe(words)
 	val sb = StringBuilder()
+	sb.append('\n').append("*****").append('\n')
 	for (w in mapp) {
 		sb.append(w.word).append(" = ").append(w.count).append('\n')
 	}
 	sb.append("Всего групп: ").append(mapp.size).append('\n')
 	println("${sb.toString()}")
-
 }
 
-private fun split(dictionaryStopWords: List<String>, words: ArrayList<String>): ArrayList<String> {
-	val wordsNew = ArrayList<String>()
-	for (index in words.size - 1 downTo 0) {
-		val w = words.get(index)
-			.trim({ it.isWhitespace() || it.equals(':') || it.equals(',') || it.equals('.') || it.equals(')') || it.equals('(') || it.equals('»') || it.equals('«') })
-		var flag = w.isNotEmpty()
-
-		for (dikWord in dictionaryStopWords) {
-			if (!flag) {
-				break
-			}
-			flag = !w.equals(dikWord, true)
-		}
-		if (flag) {
-			wordsNew.add(w)
-		}
-	}
-	wordsNew.reverse()
-	return wordsNew
-}
 
 private fun buildText(words: ArrayList<String>): String {
 	val sb = StringBuilder()
@@ -136,9 +62,6 @@ private fun buildText(words: ArrayList<String>): String {
 	return sb.toString()
 }
 
-fun split(rawText: String): ArrayList<String> {
-	return rawText.split(' ') as ArrayList<String>;
-}
 
 
 
---
+++ src/main/kotlin/domain/TextUtils.kt
@@ -0,0 +1,110 @@
+package domain
+
+import domain.entity.WordsFrequency
+import domain.entity.revSort
+import java.util.*
+
+/**
+ * Created by Zahar on 16.05.16.
+ */
+class TextUtils {
+	companion object {
+
+		fun groupe(words: ArrayList<String>): ArrayList<WordsFrequency> {
+			val list = ArrayList(words)
+
+			val groupBy = list.groupBy { it }
+			val mapp: ArrayList<WordsFrequency> = ArrayList<WordsFrequency>()
+			groupBy.forEach { mapp.add(WordsFrequency(it.key, it.value.size)) }
+			mapp.revSort()
+			mapp.removeIf { it.count == 1 }
+
+			return mapp
+		}
+
+		fun splitByWords(dictionaryStopWords: List<String>, rawText: String): ArrayList<String> {
+			val words = TextUtils.split(rawText)
+			val wordsNew = ArrayList<String>()
+			for (index in words.size - 1 downTo 0) {
+				val w = words.get(index)
+					.trim({ it.isWhitespace() || it.equals(':') || it.equals(',') || it.equals('.') || it.equals(')') || it.equals('(') || it.equals('»') || it.equals('«') })
+				var flag = w.isNotEmpty()
+
+				for (dikWord in dictionaryStopWords) {
+					if (!flag) {
+						break
+					}
+					flag = !w.equals(dikWord, true)
+				}
+				if (flag) {
+					wordsNew.add(w)
+				}
+			}
+			wordsNew.reverse()
+			return wordsNew
+		}
+
+		fun splitBySentensies(rawText: String, dictAbb: List<String>): String {
+			val carretWord = "\n*"
+
+			val sb = StringBuilder(rawText)
+			var lastIndex = -1;
+			val listChars = Arrays.asList('.', '!', '?')
+			for (ch in listChars)
+				do {
+					lastIndex = sb.indexOf(ch, lastIndex + 1)
+					val flag = lastIndex != -1;
+					if (flag) {
+						sb.insert(lastIndex + 1, carretWord)
+					}
+				} while (flag)
+
+			lastIndex = -1
+			do {
+				lastIndex = sb.indexOf(carretWord, lastIndex + 1)
+				for (word in dictAbb) {
+
+					val flag = matchStr(sb, lastIndex, word);
+					if (flag) {
+						sb.replace(lastIndex, lastIndex + carretWord.length, "")
+						break
+					}
+				}
+			} while (lastIndex != -1)
+			return sb.toString()
+		}
+
+
+		fun matchStr(sb: StringBuilder, lastIndex: Int, word: String): Boolean {
+			val length = word.length
+			var startIndex = lastIndex - length * 2
+			startIndex = if (startIndex < 0) 0 else startIndex
+			for (strIndex in startIndex..lastIndex) {
+				var flag = false
+				for (wordIndex in 0..length - 1) {
+					val c = sb[strIndex + wordIndex]
+					val c1 = word[wordIndex]
+					if (c == c1) {
+						flag = true
+					} else {
+						flag = false
+						break
+					}
+				}
+				if (flag) return true
+			}
+			return false
+		}
+
+		fun stemming(words: ArrayList<String>): ArrayList<String> {
+			val list = ArrayList<String>()
+			words.forEach { list.add(Porter.stem(it)) }
+			return list;
+		}
+
+		fun split(rawText: String): ArrayList<String> {
+			return rawText.split(' ') as ArrayList<String>;
+		}
+	}
+
+}
